45fd3b62d16d7b42d78cb335a3bd898a955d00de
Jin Young Park <jy2045@sogang.ac.kr>
2020-09-22 02:47:04 +0900

    2020/09/22 경주로건설
    * bfs에 조건이 복잡함

--- programmers/level3/2020-카카오인턴십-경주로건설.js
+++ programmers/level3/2020-카카오인턴십-경주로건설.js
@@ -1,76 +1,50 @@
-const dx = [-1, 1, 0, 0];
-const dy = [0, 0, 1, -1];
+const dx = [0, -1, 0, 1];
+const dy = [-1, 0, 1, 0];
 const STRAIGHT = 100;
 const CORNER = 500;
 
 function solution(board) {
   // bfs 돌면서 최소비용을 갱신, 중간에 최소비용보다 크면 종료.
   // 직선, 코너 구분
-  let minCost = Infinity;
+  let answer = [];
   const xLen = board.length;
   const yLen = board[0].length;
 
-  const bfs = () => {
+  const bfs = (dir) => {
     let queue = [];
-    let map = [...board];
+    let check = Array.from(Array(xLen), () => new Array(yLen).fill(0));
 
-    // dir: 0 1 2 3
-    let car = { x: 0, y: 0, dir: undefined, cost: 0 };
-
-    queue.push(car);
-    map[0][0] = 1;
+    queue.push({ x: 0, y: 0, dir, cost: 0 });
 
     while (queue.length !== 0) {
-      console.log(queue);
-      let { x, y, dir, cost } = queue.shift();
-
-      if (cost > minCost) continue;
+      const { x, y, dir, cost } = queue.shift();
 
       if (x === xLen - 1 && y === yLen - 1) {
-        minCost = Math.min(minCost, cost);
-        console.log(minCost);
-        continue;
+        answer.push(cost);
       }
 
       for (let i = 0; i < 4; i++) {
-        let [newX, newY] = [x + dx[i], y + dy[i]];
+        // 후진방향이면 pass
+        if (dir + 2 === i) continue;
 
-        // board 밖으로 벗어나면 pass
-        if (newX < 0 || newX > xLen - 1 || newY < 0 || newY > yLen - 1)
-          continue;
-        // 막혀있으면 pass
+        const [newX, newY, newDir] = [x + dx[i], y + dy[i], i];
+        let newCost = dir === i ? cost + STRAIGHT : cost + CORNER + STRAIGHT;
+
+        if (newX < 0 || newX >= xLen || newY < 0 || newY >= yLen) continue;
         if (board[newX][newY] === 1) continue;
 
-        // 이전 방향으로 돌아가면 pass
-        // if (
-        //   (dir === 0 && i === 1) ||
-        //   (dir === 1 && i === 0) ||
-        //   (dir === 2 && i === 3) ||
-        //   (dir === 3 && i === 2)
-        // )
-        //   continue;
-
-        let newDir = i;
-
-        // 비용 계산 : 출발지점이거나 방향이 같으면 직선, 그렇지 않으면 CORNER
-        let newCost =
-          cost +
-          (dir === undefined || (dir + newDir) % 2 === 0
-            ? STRAIGHT
-            : CORNER + STRAIGHT);
-
-        // 합계비용이 최소비용을 넘어서면 pass
-        if (map[newX][newY] === 0 && map[newX][newY] >= newCost) {
-          map[newX][newY] = newCost;
+        if (!check[newX][newY] || check[newX][newY] > newCost) {
+          check[newX][newY] = newCost;
           queue.push({ x: newX, y: newY, dir: newDir, cost: newCost });
         }
       }
     }
   };
 
-  bfs();
+  bfs(3);
+  bfs(2);
 
-  return minCost;
+  return Math.min(...answer);
 }
 
 const tests = [
@@ -105,7 +79,8 @@ const tests = [
   ],
 ];
 
-// for (const test of tests) {
-//   console.log(solution(test));
-// }
-console.log(solution(tests[1]));
+const answers = [900, 3800, 2100, 3200];
+
+for (const test of tests) {
+  console.log(solution(test));
+}
